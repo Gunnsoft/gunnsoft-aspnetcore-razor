bb03bf3219bcf132d188cd23700f520225269e3b
Andrew Gunn <hello@andrewgunn.co.uk>
2018-04-02 20:33:45 +0100

    Do all the things

---
+++ src/Gunnsoft.AspNetCore.Razor/TagHelpers/BootstrapValidationTagHelper.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc.Rendering;
+//using Microsoft.AspNetCore.Mvc.TagHelpers;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Stubbl.Identity.TagHelpers
+{
+    [HtmlTargetElement("input", Attributes = ForAttributeName)]
+    [HtmlTargetElement("select", Attributes = ForAttributeName)]
+    [HtmlTargetElement("textarea", Attributes = ForAttributeName)]
+    public class BootstrapValidationTagHelper : TagHelper
+    {
+        private const string ForAttributeName = "asp-for";
+
+        [HtmlAttributeName(ForAttributeName)] public ModelExpression For { get; set; }
+
+        [HtmlAttributeNotBound] [ViewContext] public ViewContext ViewContext { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            if (output == null)
+            {
+                throw new ArgumentNullException(nameof(output));
+            }
+
+            ViewContext.ViewData.ModelState.TryGetValue(For.Name, out var modelState);
+
+            if (modelState == null)
+            {
+                return;
+            }
+
+            var tagBuilder = new TagBuilder("div");
+
+            if (modelState.Errors.Any())
+            {
+                tagBuilder.AddCssClass("is-invalid");
+            }
+            else if (!string.Equals(
+                context.AllAttributes
+                    .SingleOrDefault(a => string.Equals(a.Name, "type", StringComparison.InvariantCultureIgnoreCase))
+                    ?.Value?.ToString(), "password", StringComparison.InvariantCulture))
+            {
+                tagBuilder.AddCssClass("is-valid");
+            }
+
+            foreach (var attribute in tagBuilder.Attributes)
+            {
+                var name = Convert.ToString(attribute.Key, CultureInfo.InvariantCulture);
+                var value = Convert.ToString(attribute.Value, CultureInfo.InvariantCulture);
+
+                output.Attributes.SetAttribute(name, value);
+            }
+        }
+    }
+}
